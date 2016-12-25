5f8c0a32b38bf2ea14b69ff6ed067959592ead67
wg2 <khan.will1234@gmail.com>
2016-12-25 12:49:24 -0800

    fixed spacing

---
+++ city_loop.py
@@ -0,0 +1,21 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import random
+mc = minecraft.Minecraft.create()
+
+#get position of player
+pos = mc.player.getTilePos()            
+
+#put first building 2 blocks x from player position
+x = pos.x+2
+y = pos.y
+z = pos.z
+
+height = random.randint(y+5,y+20)        #random building height
+
+#loop to create row of randomly spaced buildings with random height
+for loop in range(5):
+    mc.setBlocks(x, y, z, x+1, height, z+1, block.GLASS.id)
+    x = x + random.randint(5,10)    #randomly space next buildings
+    height = random.randint(y+5,y+20)        #change next building height
+
